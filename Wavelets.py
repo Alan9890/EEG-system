8df22c69ec6cb1b7690d37157455caa7017f2017
MProx <34684113+MProx@users.noreply.github.com>
2017-12-19 18:28:57 +0200

    Create Wavelets.py
    Experimental script that pulls data from c:\EEG.txt, and cleans it using a 3rd-level wavelet transform and soft-thresholding on the detail coefficients. The threshold is set to 15% of the respective maximums for each coefficient array (one at each level).

---
+++ Wavelets.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import pywt
+
+file_object  = open('C:\EEG.txt', 'r')
+raw_data = file_object.readlines()
+
+index = []
+data = []
+
+for i in range(len(raw_data)-1):
+    x, y = raw_data[i].split("\t")
+    X = float(x)
+    Y = float(y)
+    index.append(X)
+    data.append(Y)
+
+[cA, cD3, cD2, cD1] = pywt.wavedec(data, 'sym4', level=3)
+
+threshold = 0.15
+
+plt.figure()
+
+plt.subplot(3, 1, 1)
+plt.title("Wavelet transform coefficients: cD1")
+plt.plot(cD1)
+cD1 = pywt.threshold(cD1, threshold*max(cD1))
+plt.plot(cD1)
+
+plt.subplot(3, 1, 2)
+plt.title("Wavelet transform coefficients: cD2")
+plt.plot(cD2)
+cD2 = pywt.threshold(cD2, threshold*max(cD2))
+plt.plot(cD2)
+
+plt.subplot(3, 1, 3)
+plt.title("Wavelet transform coefficients: cD3")
+plt.plot(cD3)
+cD3 = pywt.threshold(cD3, threshold*max(cD3))
+plt.plot(cD3)
+
+plt.tight_layout()
+plt.show()
+
+datarec = pywt.waverec([cA, cD3, cD2, cD1], 'sym4')
+
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(index[:15000], data[:15000])
+plt.xlabel('time (s)')
+plt.ylabel('microvolts (uV)')
+plt.title("Raw signal")
+plt.ylim([-130,400])
+plt.subplot(2, 1, 2)
+plt.plot(index[:15000], datarec[:15000])
+plt.xlabel('time (s)')
+plt.ylabel('microvolts (uV)')
+plt.title("De-noised signal using wavelet techniques")
+plt.ylim([-130,400])
+
+plt.tight_layout()
+plt.show()
